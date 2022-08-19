80a1aed50b13579fa46fd0a86f5d613f44ee4433
Darya <HavrylenkoIT@gmail.com>
2022-08-20 00:23:54 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+      if(email.value.length === 0 || password.value.length === 0) {
+        alert('все поля должны быть заполнены!');
+    }
+    console.log({ email: email.value, password: password.value });
+    form.reset();
+});
